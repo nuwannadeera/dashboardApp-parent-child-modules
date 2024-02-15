96583a66175f1994a20822c33e471173b3148b59
Nuwan Nadeera <47295198+nuwannadeera@users.noreply.github.com>
2024-02-15 17:20:45 +0530

    how to remove duplicate in array

--- src/app/child-component/child-component.component.ts
+++ src/app/child-component/child-component.component.ts
@@ -12,4 +12,17 @@ export class ChildComponentComponent {
   @Input() name1: any;
   @Input() obj: any;
 
+  array = [1, 2, 3, 4, 1, 2, 3, 4, 8, 9];
+  newArr: any = [];
+
+  ngOnInit() {
+// foreach to remove duplicate in array
+    this.array.forEach((no, index) => {
+      if (this.array.indexOf(no) === index) {
+        this.newArr.push(no);
+      }
+    });
+    console.log('newArr');
+    console.log(this.newArr);
+  }
 }
